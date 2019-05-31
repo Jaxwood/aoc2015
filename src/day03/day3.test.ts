f552f382bdc46331abcee7bd2ea75b066c55153c
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-05-31 18:09:54 +0200

    Solution for day3b

--- src/day03/day3.test.ts
+++ src/day03/day3.test.ts
@@ -1,12 +1,19 @@
 import fsReadfilePromise from 'fs-readfile-promise';
 import path from 'path';
-import { day3a } from './day3';
+import { day3a, day3b } from './day3';
 
 describe('day3', () => {
   const testcasesA = [['>', '2'], ['^>v<', '4'], ['^v^v^v^v^v', '2']];
+  const testcasesB = [['^v', '3'], ['^>v<', '3'], ['^v^v^v^v^v', '11']];
   describe.each(testcasesA)('a', (input, expected) => {
     it('should calculate size', () => {
       expect(day3a(input)).toBe(parseInt(expected, 10));
+    });
+  });
+
+  describe.each(testcasesB)('b', (input, expected) => {
+    it('should calculate size', () => {
+      expect(day3b(input)).toBe(parseInt(expected, 10));
     });
   });
 
@@ -17,5 +24,14 @@ describe('day3', () => {
     );
     const actual = day3a(input);
     expect(actual).toBe(2081);
+  });
+
+  it('should calculate solution for day3b', async () => {
+    const input = await fsReadfilePromise(
+      path.resolve(__dirname, 'input.txt'),
+      'utf-8'
+    );
+    const actual = day3b(input);
+    expect(actual).toBe(2341);
   });
 });
--- src/day03/day3.ts
+++ src/day03/day3.ts
@@ -3,21 +3,45 @@ export function day3a(input: string): number {
   let current = [0, 0];
   houses.add(JSON.stringify(current));
   for (const house of input) {
-    const [x, y] = current;
-    if (house === '<') {
-      current = [x - 1, y];
-    }
-    if (house === '>') {
-      current = [x + 1, y];
-    }
-    if (house === 'v') {
-      current = [x, y - 1];
-    }
-    if (house === '^') {
-      current = [x, y + 1];
-    }
+    current = move(current, house);
     houses.add(JSON.stringify(current));
   }
 
   return houses.size;
+}
+
+export function day3b(input: string): number {
+  const houses = new Set<string>();
+  let santa = [0, 0];
+  let roboSanta = [0, 0];
+  houses.add(JSON.stringify(santa));
+
+  for (let i = 0; i < input.length; i++) {
+    if (i % 2 === 0) {
+      santa = move(santa, input[i]);
+      houses.add(JSON.stringify(santa));
+    } else {
+      roboSanta = move(roboSanta, input[i]);
+      houses.add(JSON.stringify(roboSanta));
+    }
+  }
+
+  return houses.size;
+}
+
+function move(current: number[], house: string) {
+  const [x, y] = current;
+  if (house === '<') {
+    current = [x - 1, y];
+  }
+  if (house === '>') {
+    current = [x + 1, y];
+  }
+  if (house === 'v') {
+    current = [x, y - 1];
+  }
+  if (house === '^') {
+    current = [x, y + 1];
+  }
+  return current;
 }
