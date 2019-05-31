8a69e70955281f4442a5317ed70ab0e647c3a20d
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-05-31 14:10:19 +0200

    Refactor test cases

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,61 +1,26 @@
 import fsReadfilePromise from 'fs-readfile-promise';
-import { Sut } from './day1';
+import { day1 } from './day1';
 
-interface TestCase {
-  expected: number;
-  pattern: string;
-}
 describe('day1', () => {
-  it('should calculate floors', () => {
-    const sut = new Sut();
-    const input: TestCase[] = [
-      {
-        expected: 0,
-        pattern: '(())',
-      },
-      {
-        expected: 0,
-        pattern: '()()',
-      },
-      {
-        expected: 3,
-        pattern: '(((',
-      },
-      {
-        expected: 3,
-        pattern: '(()(()(',
-      },
-      {
-        expected: 3,
-        pattern: '))(((((',
-      },
-      {
-        expected: -1,
-        pattern: '())',
-      },
-      {
-        expected: -1,
-        pattern: '))(',
-      },
-      {
-        expected: -3,
-        pattern: ')))',
-      },
-      {
-        expected: -3,
-        pattern: ')())())',
-      },
-    ];
-    for (const testcase of input) {
-      const actual = sut.day1(testcase.pattern);
-      expect(actual).toBe(testcase.expected);
-    }
+  const testCases = [
+    ['(((', '3'],
+    ['()()', '0'],
+    ['(()(()(', '3'],
+    ['(())', '0'],
+    ['())', '-1'],
+    ['))(', '-1'],
+    [')))', '-3'],
+    [')())())', '-3'],
+  ];
+  describe.each(testCases)('%s', (input, expected) => {
+    it(`should equal ${expected}`, () => {
+      expect(day1(input)).toBe(parseInt(expected, 10));
+    });
   });
 
-  it('should calculate floors to day1 input', async () => {
-    const sut = new Sut();
+  it('day1 input should equal 280', async () => {
     const input = await fsReadfilePromise('./src/day1/input.txt', 'utf-8');
-    const actual = sut.day1(input);
+    const actual = day1(input);
     expect(actual).toBe(280);
   });
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,13 +1,11 @@
-export class Sut {
-  day1(input: string): number {
-    let cnt = 0;
-    for (const s of input) {
-      if (s === '(') {
-        cnt = cnt + 1;
-      } else {
-        cnt = cnt - 1;
-      }
+export function day1(input: string): number {
+  let cnt = 0;
+  for (const s of input) {
+    if (s === '(') {
+      cnt = cnt + 1;
+    } else {
+      cnt = cnt - 1;
     }
-    return cnt;
   }
+  return cnt;
 }
