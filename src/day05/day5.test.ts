2fa443822846e550955512b4f859f2c8425a55d0
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-05-31 20:19:24 +0200

    Solution for day5b

--- src/day05/day5.test.ts
+++ src/day05/day5.test.ts
@@ -1,25 +1,49 @@
 import fsReadfilePromise from 'fs-readfile-promise';
 import path from 'path';
-import { day5a } from './day5';
+import { day5a, day5b } from './day5';
 
 describe('day5', () => {
-  it('should calculate', () => {
-    const input: string[] = [
-      'ugknbfddgicrmopn',
-      'aaa',
-      'jchzalrnumimnmhp',
-      'haegwjzuvuyypxyu',
-      'dvszwmarrgswjxmb',
-    ];
-    expect(day5a(input)).toBe(2);
+  describe('a', () => {
+    it('should calculate nice words', () => {
+      const input: string[] = [
+        'ugknbfddgicrmopn',
+        'aaa',
+        'jchzalrnumimnmhp',
+        'haegwjzuvuyypxyu',
+        'dvszwmarrgswjxmb',
+      ];
+      expect(day5a(input)).toBe(2);
+    });
+
+    it('should calculate solution for day5a', async () => {
+      const input = await fsReadfilePromise(
+        path.resolve(__dirname, 'input.txt'),
+        'utf-8'
+      );
+      const actual = day5a(input.split('\n'));
+      expect(actual).toBe(255);
+    });
   });
 
-  it('should calculate solution for day5a', async () => {
-    const input = await fsReadfilePromise(
-      path.resolve(__dirname, 'input.txt'),
-      'utf-8'
-    );
-    const actual = day5a(input.split('\n'));
-    expect(actual).toBe(255);
+  describe('b', () => {
+    it('should calculate nice words', () => {
+      const input: string[] = [
+        'qjhvhtzxzqqjkmpb',
+        'xxyxx',
+        'uurcxstgmygtbstg',
+        'ieodomkazucvgmuy',
+        'aaa',
+      ];
+      expect(day5b(input)).toBe(2);
+    });
+
+    it('should calculate solution for day5b', async () => {
+      const input = await fsReadfilePromise(
+        path.resolve(__dirname, 'input.txt'),
+        'utf-8'
+      );
+      const actual = day5b(input.split('\n'));
+      expect(actual).toBe(55);
+    });
   });
 });
--- src/day05/day5.ts
+++ src/day05/day5.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 const vowels = 'aeiou';
 const disallowed = ['ab', 'cd', 'pq', 'xy'];
 
@@ -13,6 +14,43 @@ export function day5a(input: string[]): number {
     }
   }
   return result;
+}
+
+export function day5b(input: string[]): number {
+  let result = 0;
+  for (const candidate of input) {
+    if (hasPair(candidate) && hasRepeatingLetter(candidate)) {
+      result += 1;
+    }
+  }
+  return result;
+}
+
+function hasPair(candidate: string): boolean {
+  const pairs = new Set<string>();
+  const prevPair = new Map<string, number>();
+  for (let i = 0; i < candidate.length - 1; i++) {
+    const key = JSON.stringify([candidate[i], candidate[i + 1]]);
+    if (pairs.has(key)) {
+      const prevIndex = prevPair.get(key) || 0;
+      if (i !== prevIndex + 1) {
+        return true;
+      }
+    } else {
+      prevPair.set(key, i);
+      pairs.add(key);
+    }
+  }
+  return false;
+}
+
+function hasRepeatingLetter(candidate: string): boolean {
+  for (let i = 1; i < candidate.length - 1; i++) {
+    if (candidate[i - 1] === candidate[i + 1]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function hasDisallowed(input: string): boolean {
