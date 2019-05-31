0ca20a2bd247b9d98997da9eaa18e24550a1515f
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-05-31 16:36:06 +0200

    Solution for day2b

--- src/day02/day2.test.ts
+++ src/day02/day2.test.ts
@@ -1,21 +1,37 @@
 import fsReadfilePromise from 'fs-readfile-promise';
 import path from 'path';
-import { day2a } from './day2';
+import { day2a, day2b } from './day2';
 
-describe('day2a', () => {
-  const testcases = [['2x3x4', '58'], ['1x1x10', '43']];
-  describe.each(testcases)('', (input, expected) => {
+describe('day2', () => {
+  const testcasesA = [['2x3x4', '58'], ['1x1x10', '43']];
+  const testcasesB = [['4x3x2', '34'], ['1x10x1', '14']];
+  describe.each(testcasesA)('a', (input, expected) => {
     it('should calculate size', () => {
       expect(day2a([input])).toBe(parseInt(expected, 10));
     });
   });
 
-  it('should calculate soultion', async () => {
+  describe.each(testcasesB)('b', (input, expected) => {
+    it('should calculate size', () => {
+      expect(day2b([input])).toBe(parseInt(expected, 10));
+    });
+  });
+
+  it('should calculate soultion for a', async () => {
     const fileContent = await fsReadfilePromise(
       path.resolve(__dirname, 'input.txt'),
       'UTF-8'
     );
     const lines = fileContent.split('\n');
     expect(day2a(lines)).toBe(1588178);
+  });
+
+  it('should calculate soultion for b', async () => {
+    const fileContent = await fsReadfilePromise(
+      path.resolve(__dirname, 'input.txt'),
+      'UTF-8'
+    );
+    const lines = fileContent.split('\n');
+    expect(day2b(lines)).toBe(3800576);
   });
 });
--- src/day02/day2.ts
+++ src/day02/day2.ts
@@ -1,14 +1,30 @@
 import * as _ from 'lodash';
 
-export function day2a(input: string[]) {
-  return _.sum(input.map(l => sum(l)));
+export function day2a(input: string[]): number {
+  return _.sum(input.map(paperSize));
 }
 
-function sum(input: string): number {
-  const [l, w, h] = input.split('x').map(s => parseInt(s, 10));
+export function day2b(input: string[]): number {
+  return _.sum(input.map(ribonSize));
+}
+
+function toInt(numberAsString: string): number {
+  return parseInt(numberAsString, 10);
+}
+
+function paperSize(input: string): number {
+  const [l, w, h] = input.split('x').map(toInt);
   const side1 = 2 * l * w;
   const side2 = 2 * w * h;
   const side3 = 2 * h * l;
   const minside = _.min([l * w, w * h, h * l]) || 0;
   return side1 + side2 + side3 + minside;
+}
+
+function ribonSize(input: string): number {
+  const [l, w, h] = input
+    .split('x')
+    .map(toInt)
+    .sort();
+  return 2 * l + 2 * w + l * w * h;
 }
