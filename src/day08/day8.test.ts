8601957a4b307ee681048a085098b7722eb63bfe
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-06-06 16:14:53 +0200

    Use eval

--- src/day08/day8.test.ts
+++ src/day08/day8.test.ts
@@ -1,14 +1,25 @@
 // tslint:disable: quotemark
+import fsReadfilePromise from 'fs-readfile-promise';
+import path from 'path';
 import { day8a } from './day8';
 
 describe('day8', () => {
-  it('should calculate characters', () => {
-    const input = [
-      "",
-      "abc",
-      "aaa\"aaa",
-      "\x27",
-    ];
-    expect(day8a(input)).toBe(12);
+  it('should calculate characters', async () => {
+    const input = await fsReadfilePromise(
+      path.resolve(__dirname, 'a.txt'),
+      'utf-8'
+    );
+    const actual = day8a(input.split('\r\n'));
+    expect(actual).toBe(12);
+  });
+
+  it('should get the solution to day8a', async () => {
+    const input = await fsReadfilePromise(
+      path.resolve(__dirname, 'input.txt'),
+      'utf-8'
+    );
+    // remove start and end qoute
+    const actual = day8a(input.split('\r\n'));
+    expect(actual).toBe(1350);
   });
 });
--- src/day08/day8.ts
+++ src/day08/day8.ts
@@ -1,7 +1,7 @@
 export function day8a(input: string[]): number {
   let sum = 0;
   for (const str of input) {
-    sum += characterCodes(str) - str.length;
+    sum += str.length - (eval(str)).length;
   }
   return sum;
 }
@@ -9,18 +9,7 @@ export function day8a(input: string[]): number {
 function characterCodes(input: string) {
   let cnt = 2;
   for (const c of input) {
-    const ascii = c.charCodeAt(0);
-    if (ascii >= 96 && ascii <= 122) {
-      cnt++;
-    }
-    else {
-      if (ascii === 34 || ascii === 10) {
-        cnt += 2;
-      }
-      else {
-        cnt += 4;
-      }
-    }
+    cnt++;
   }
   return cnt;
 }
