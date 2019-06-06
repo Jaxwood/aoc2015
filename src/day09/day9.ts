0106a31ec57a45725ec7e9686fb074b3ecb7868e
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-06-06 16:44:22 +0200

    Parse input

--- src/day09/day9.ts
+++ src/day09/day9.ts
@@ -1,3 +1,22 @@
+const regex = new RegExp(/(\w+) to (\w+) = (\d+)/);
+
+interface ITravel {
+  cost: number,
+  from: string,
+  to: string,
+}
+
 export function day9a(input: string[]) {
+  const nodes: ITravel[] = [];
+  for (const line of input) {
+    if (regex.test(line)) {
+      const match = line.match(regex);
+      if (match) {
+      const [exp, from, to, cost] = match;
+        nodes.push({from, to, cost: parseInt(cost, 10) });
+      }
+    }
+  }
+
   return 0;
 }
