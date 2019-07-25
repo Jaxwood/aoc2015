049df5b57112215cf94adc3c24b68c83000615ed
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-07-25 22:08:25 +0200

    traverse document

--- src/day12/day12.test.ts
+++ src/day12/day12.test.ts
@@ -2,6 +2,36 @@ import { day12a } from './day12';
 
 describe('day12', () => {
   it('should calculate sum for [1,2,3]', async () => {
-    expect(day12a([1,2,3])).toBe(6);
+    const actual = day12a([1,2,3]);
+    expect(actual).toBe(6);
+  });
+
+  it('should calculate sum for {"a":2,"b":4}', async () => {
+    const actual = day12a({'a':2,'b':4});
+    expect(actual).toBe(6);
+  });
+
+  it('should calculate sum for [[[3]]]', async () => {
+    expect(day12a([[[3]]])).toBe(3);
+  });
+
+  it('should calculate sum for {"a":{"b":4},"c":-1}', async () => {
+    expect(day12a({'a':{'b':4},'c':-1})).toBe(3);
+  });
+
+  it('should calculate sum for {"a":[-1,1]}', async () => {
+    expect(day12a({'a':[-1,1]})).toBe(0);
+  });
+
+  it('should calculate sum for [-1,{"a":1}]', async () => {
+    expect(day12a([-1,{'a':1}])).toBe(0);
+  });
+
+  it('should calculate sum for []', async () => {
+    expect(day12a([])).toBe(0);
+  });
+
+  it('should calculate sum for {}', async () => {
+    expect(day12a({})).toBe(0);
   });
 });
--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -1,3 +1,18 @@
+import * as _ from 'lodash';
+
 export function day12a(input: any): number {
-  return 0;
+  let sum = 0;
+  if (input instanceof Array) {
+    for (const i of input) {
+      if (_.isNumber(i)) {
+        sum += i;
+      }
+      else {
+        sum += day12a(i);
+      }
+    }
+  } else {
+    sum += day12a(Object.values(input));
+  }
+  return sum;
 }
