7ce2ecde5a093f7863bee754e284b4ee92d8243e
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-07-27 06:55:22 +0200

    Rough implementation

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -1,13 +1,37 @@
+import * as _ from 'lodash';
 const reg = new RegExp(/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)/)
 
-type Person = [string, Happiness, number];
-enum Happiness {
-  Gain,
-  Lose
-}
+type Person = [string, number];
 
 export function day13(input: string[]) {
   const seatingPreferences = parseInput(input);
+  const seatingOptions = permute([...seatingPreferences.keys()]);
+  const costs = seatingOptions.map(c => calculate(c, seatingPreferences));
+  return _.max(costs);
+}
+
+function calculate(seatings: string[], seatingPreference: Map<string, Person[]>): number {
+  let sum = 0;
+  for (let i = 0; i < seatings.length; i++) {
+    const key = seatings[i];
+    const preferences = seatingPreference.get(key) || [];
+    const left = i === 0 ? seatings.length - 1 : i - 1;
+    const right = i === seatings.length - 1 ? 0 : i + 1;
+    const leftperson = getScore(preferences, seatings[left]);
+    const rightperson = getScore(preferences, seatings[right]);
+    sum += leftperson + rightperson;
+  }
+
+  return sum;
+}
+
+function getScore(preferences: Person[], key: string): number {
+  for (const person of preferences) {
+    const [name,  score] = person;
+    if (key === name) {
+      return score;
+    }
+  }
   return 0;
 }
 
@@ -18,13 +42,37 @@ function parseInput(input: string[]): Map<string, Person[]> {
     if (match) {
       const [_, source, type, score, target] = match;
       const persons = seatingPreferences.get(source) || [];
+      const modifier = type === 'gain' ? 1 : -1;
       persons.push([
         target,
-        type === 'gain' ? Happiness.Gain : Happiness.Lose,
-        parseInt(score, 10),
+        modifier * parseInt(score, 10),
       ]);
       seatingPreferences.set(source, persons);
     }
   }
   return seatingPreferences;
+}
+
+// taken from https://stackoverflow.com/a/37580979/99928
+function permute(permutation: string[]): string[][] {
+  const length = permutation.length;
+  const result = [permutation.slice()];
+  const c = new Array(length).fill(0);
+  let idx = 1;
+
+  while (idx < length) {
+    if (c[idx] < idx) {
+      const k = idx % 2 && c[idx];
+      const p = permutation[idx];
+      permutation[idx] = permutation[k];
+      permutation[k] = p;
+      ++c[idx];
+      idx = 1;
+      result.push(permutation.slice());
+    } else {
+      c[idx] = 0;
+      ++idx;
+    }
+  }
+  return result;
 }
