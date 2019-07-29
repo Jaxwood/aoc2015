801a9738ecea20756e2bcbbc12025923736d2851
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-07-29 21:57:07 +0200

    Add day17 setup

---
+++ src/day17/day17.test.ts
@@ -0,0 +1,9 @@
+import { day17a } from './day17';
+
+describe('day17', () => {
+  describe('a', () => {
+    it('should find max combinations', () => {
+      expect(day17a(25)).toBe(4)
+    });
+  });
+});
---
+++ src/day17/day17.ts
@@ -0,0 +1,5 @@
+const containers = [20, 15, 10, 5, 5];
+
+export function day17a(liters: number): number {
+  return 0;
+}
