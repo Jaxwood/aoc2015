996fcec8930006ef6326b16c8a84d7b80ce78c4c
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-08-02 18:26:15 +0200

    Find min steps

--- src/day17/day17.test.ts
+++ src/day17/day17.test.ts
@@ -1,6 +1,6 @@
 import fsReadfilePromise from 'fs-readfile-promise';
 import path from 'path';
-import { day17a } from './day17';
+import { day17a, day17b } from './day17';
 
 describe('day17', () => {
   describe('a', () => {
@@ -17,6 +17,23 @@ describe('day17', () => {
       );
       const actual = day17a(150, input.split('\r\n').map(c => parseInt(c, 10)));
       expect(actual).toBe(1638);
+    });
+  });
+
+  describe('b', () => {
+    it('should find max combinations', () => {
+      const liters = 25;
+      const containers = [20, 10, 15, 5, 5];
+      expect(day17b(liters, containers)).toBe(3)
+    });
+
+    it('should find max combinations for puzzle input', async () => {
+      const input = await fsReadfilePromise(
+        path.resolve(__dirname, 'input.txt'),
+        'utf-8'
+      );
+      const actual = day17b(150, input.split('\r\n').map(c => parseInt(c, 10)));
+      expect(actual).toBe(17);
     });
   });
 });
--- src/day17/day17.ts
+++ src/day17/day17.ts
@@ -8,6 +8,28 @@ export function day17a(liters: number, containers: number[]): number {
     return 0;
   }
   else {
-    return day17a(liters - containers[0], containers.slice(1)) + day17a(liters, containers.slice(1))
+    return day17a(liters - containers[0], containers.slice(1)) + day17a(liters, containers.slice(1));
+  }
+}
+
+export function day17b(liters: number, containers: number[]): number {
+  const combinations = combine(liters, containers, [], []);
+  const minLength = (_.minBy(combinations, c => c.length) || []).length;
+  if (minLength) {
+    return combinations.filter(c => c.length === minLength).length;
+  }
+  return 0;
+}
+
+function combine(liters: number, containers: number[], steps: number[], result: number[][]): number[][] {
+  if (liters === 0) {
+    result.push(steps);
+    return result;
+  }
+  else if (containers.length === 0 || liters < 0) {
+    return [];
+  }
+  else {
+    return combine(liters - containers[0], containers.slice(1), steps.concat(containers[0]), result).concat(combine(liters, containers.slice(1), steps, []));
   }
 }
