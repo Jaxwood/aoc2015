5b9a1e5be4d139ef76e70f03de9e5e8603783f89
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-08-02 09:16:33 +0200

    Find possible combinations

--- src/day17/day17.test.ts
+++ src/day17/day17.test.ts
@@ -1,9 +1,22 @@
+import fsReadfilePromise from 'fs-readfile-promise';
+import path from 'path';
 import { day17a } from './day17';
 
 describe('day17', () => {
   describe('a', () => {
     it('should find max combinations', () => {
-      expect(day17a(25)).toBe(4)
+      const liters = 25;
+      const containers = [20, 10, 15, 5, 5];
+      expect(day17a(liters, containers)).toBe(4)
+    });
+
+    it('should find max combinations for puzzle input', async () => {
+      const input = await fsReadfilePromise(
+        path.resolve(__dirname, 'input.txt'),
+        'utf-8'
+      );
+      const actual = day17a(150, input.split('\r\n').map(c => parseInt(c, 10)));
+      expect(actual).toBe(1638);
     });
   });
 });
--- src/day17/day17.ts
+++ src/day17/day17.ts
@@ -1,5 +1,13 @@
-const containers = [20, 15, 10, 5, 5];
+import * as _ from 'lodash';
 
-export function day17a(liters: number): number {
-  return 0;
+export function day17a(liters: number, containers: number[]): number {
+  if (liters === 0) {
+    return 1;
+  }
+  else if (containers.length === 0 || liters < 0) {
+    return 0;
+  }
+  else {
+    return day17a(liters - containers[0], containers.slice(1)) + day17a(liters, containers.slice(1))
+  }
 }
